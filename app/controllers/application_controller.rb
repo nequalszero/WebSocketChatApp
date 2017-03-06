d696b00f3c99a009f26943718bd59d81f6e45a1f
nequalszero <nequalszero@gmail.com>
2017-03-05 16:19:55 -0800

    Unnest model from api folder, complete back end auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  # helper_method :current_user
+
+  private
+  def current_user
+    User.find_by_session_token(session[:session_token])
+  end
+
+  def sign_in(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out
+    current_user.try(:reset_session_token!)
+    session[:session_token] = nil
+  end
 end
